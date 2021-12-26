cd21eef4df561b30f91cf4a02f6b3e153e93d203
trademkose <tr.ademkose@gmail.com>
2021-12-26 17:21:27 +0300

    update the function

--- myfirstfunction.py
+++ myfirstfunction.py
@@ -1,33 +1,46 @@
 from pulsar import Function
 import json
 from random import random
-current_tl_usd_currency=14.5
-class RoutingFunction(Function):
+class MyFirstFunction(Function):
     def __init__(self):
-
+        f=open("functionlogs.txt","w")
+        f.write("MyFirstFunction started!" + "\n")
+        f.close()
+        self.current_tl_usd_currency=14.5
         self.smartphone_topic = "persistent://public/default/smartphone"
         self.computer_topic = "persistent://public/default/computer"
 
     def process(self, item, context):
         f=open("functionlogs.txt","a")
-        f.write("ITEM" + "\n")
+        f.write("INPUT ITEM" + "\n")
         f.write(item + "\n")
-        
         data = json.loads(item)
-        price_usd=data["price_usd"]
-        price_tl = int(price_usd)*current_tl_usd_currency
-        data["price_tl"]= price_tl    
+        price_usd = data["price_usd"]
+        price_tl = int(price_usd)*self.current_tl_usd_currency
+        data["price_tl"] = price_tl
         if data["category_name"]=="smartphone":
-            data["imei"] = int(random()*10000)
-            context.publish(self.smartphone_topic, data)
+                data["imei"] = int(random()*10000)
+                f.write("INPUT is smartphone" + "\n")
+
+                context.publish(self.smartphone_topic, json.dumps(data))
         elif data["category_name"]=="computer" :
-            data["mac_adress"] = int(random()*100000000)
-            context.publish(self.computer_topic, data)             
+                data["mac_adress"] = int(random()*100000000)
+                f.write("INPUT is computer" + "\n")
+                context.publish(self.computer_topic, json.dumps(data))
         else:
-            warning = "The item {0} is neither a fruit nor a vegetable".format(>
-            context.get_logger().warn(warning)
-        f.write(warning + "\n")
-        f.write(data)
+                warning = "The item {0} is neither a smartphone nor a computer".format(item)
+                context.get_logger().warn(warning)
+                f.write(warning + "\n")
+        f.write("OUTPUT ITEM" + "\n")
+        f.write(json.dumps(data) + "\n")
         f.close()
 
+
+./bin/pulsar-admin functions delete --tenant public --namespace default --name myfirstfunction
+
+
+./bin/pulsar-admin functions list --tenant public --namespace default
+
+
+bin/pulsar-admin functions create --py myfirstfunction.py --classname myfirstfunction.MyFirstFunction --tenant public --namespace default --name myfirstfunction --inputs persistent://public/default/topic-new-product
 
