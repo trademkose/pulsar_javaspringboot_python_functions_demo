8f1691b644a6af71b04e8136552cf2c5a06bc430
Adem KOSE <tr.ademkose@gmail.com>
2022-01-14 23:58:33 +0300

    update function

--- myfirstfunction.py
+++ myfirstfunction.py
@@ -1,53 +1,33 @@
 from pulsar import Function
-
+import json
+from random import random
+current_tl_usd_currency=14.5
 class RoutingFunction(Function):
     def __init__(self):
-        f=open("functionlogs.txt","a")
-        f.write("INIT        " + "\n")
-        f.close()
 
-        self.fruits_topic = "persistent://public/default/fruits"
-        self.vegetables_topic = "persistent://public/default/vegetables"
-
-    @staticmethod
-    def is_fruit(item):
-        f=open("functionlogs.txt","a")
-        f.write("is_fruit        " + "\n")
-        f.close()
-
-        return item in ["apple", "orange","pear", "other fruits..."]
-
-    @staticmethod
-    def is_vegetable(item):
-        f=open("functionlogs.txt","a")
-        f.write("is_vegetable        " + "\n")
-        f.close()
-
-        return item in ["carrot", "lettuce", "radish", "other vegetables..."]
+        self.smartphone_topic = "persistent://public/default/smartphone"
+        self.computer_topic = "persistent://public/default/computer"
 
     def process(self, item, context):
         f=open("functionlogs.txt","a")
-        f.write("=======process" + "\n")
+        f.write("ITEM" + "\n")
         f.write(item + "\n")
-
-
-        if self.is_fruit(item):
-            f.write("fruit\n")
-
-            context.publish(self.fruits_topic, item)
-        elif self.is_vegetable(item):
-            f.write("self.is_vegetable \n")
-
-            context.publish(self.vegetables_topic, item)
+        
+        data = json.loads(item)
+        price_usd=data["price_usd"]
+        price_tl = int(price_usd)*current_tl_usd_currency
+        data["price_tl"]= price_tl    
+        if data["category_name"]=="smartphone":
+            data["imei"] = int(random()*10000)
+            context.publish(self.smartphone_topic, data)
+        elif data["category_name"]=="computer" :
+            data["mac_adress"] = int(random()*100000000)
+            context.publish(self.computer_topic, data)             
         else:
             warning = "The item {0} is neither a fruit nor a vegetable".format(>
             context.get_logger().warn(warning)
-        f=open("functionlogs.txt","a")
         f.write(warning + "\n")
-        f.write(self.fruits_topic + "\n")
-        f.write(self.fruits_topic + "\n")
-
-        f.write(item)
+        f.write(data)
         f.close()
 
 
